6fc25b860d6f6eba1c62247c33622d535d123c68
kimin3 <kmns0829@gmail.com>
2025-11-23 22:00:55 +0900

    [add]: add sample page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,17 +7,27 @@ import ChatPage from '../pages/ChatPage';
 import MorePage from '../pages/MorePage';
 import LoginPage from '../pages/LoginPage';
 import Layout from '../components/layout/Layout';
+import { useEffect } from "react";
+
+function RedirectToStatic() {
+  useEffect(() => {
+    window.location.href = "/meomum/sample.html";
+  }, []);
+
+  return null;
+}
 
 function Router() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route element={<Layout />}>
-            <Route path='/' element={<HomePage />} />
+            <Route path="/" element={<RedirectToStatic />} /> 
+            {/* <Route path='/' element={<HomePage />} />
             <Route path='/community' element={<CommunityPage />}/>
             <Route path='/reservation' element={<ReservationPage />}/>
             <Route path='/chat' element={<ChatPage />}/>
-            <Route path='/more' element={<MorePage />}/>
+            <Route path='/more' element={<MorePage />}/> */}
           </Route>
 
           <Route path='/login' element={<LoginPage />} />
